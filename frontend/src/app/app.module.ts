09126162886ba2f242c417d17699d3dfcab9c545
Denise-Phi Khuu <denisekhuu@hotmail.de>
2021-10-27 19:18:28 +0200

    feature/webpages/aboutus and home

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutusComponent
   ],
   imports: [
     BrowserModule,
