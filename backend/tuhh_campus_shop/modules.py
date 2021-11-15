5b91bd75092f833d9e10fb6814090a4d2541887b
Yossef1207 <84873932+Yossef1207@users.noreply.github.com>
2021-11-15 18:39:07 +0100

    Delete modules.py

--- backend/tuhh_campus_shop/modules.py
+++
@@ -1,9 +0,0 @@
-class ProductQuerySet(models.QuerySet):
-
-#### Yossef
-    def Get_Product_Description(self, product_ID):
-        return self.filter(pk=product_ID).first().product_Description
-        
-    def Get_Product_Picture_Path(self, product_ID):
-        return self.filter(pk=product_ID).first().picture_Path
-####
