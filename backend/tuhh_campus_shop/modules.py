e760b01f63caf2463645494f4d0b092eb2b43c02
Yossef1207 <yossef.albuni@tuhh.de>
2021-11-15 18:09:43 +0100

    corrected some values

--- backend/tuhh_campus_shop/modules.py
+++ backend/tuhh_campus_shop/modules.py
@@ -1,14 +1,9 @@
 class ProductQuerySet(models.QuerySet):
 
-    # no need for a constructor
-    # Python to SQL
-
-    """+ string: Get_Product_Description(product_ID)"""
+#### Yossef
     def Get_Product_Description(self, product_ID):
         return self.filter(pk=product_ID).first().product_Description
         
-    """+ string: Get_Product_Picture_Path(product_ID)"""
     def Get_Product_Picture_Path(self, product_ID):
         return self.filter(pk=product_ID).first().picture_Path
-
-    # other miro methods...
+####
