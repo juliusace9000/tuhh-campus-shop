4950f77a7e1e287f77bd867fc7e65e9d528583b2
unknown <razvandra33@gmail.com>
2021-11-16 19:26:16 +0100

    connected to database and extracted models

--- backend/tuhh_campus_shop/models.py
+++ backend/tuhh_campus_shop/models.py
@@ -17,9 +17,34 @@ class ProductQuerySet(models.QuerySet):
 #####
 
 
-# don't forget productQueryManager = ProductQuerySet.as_manager()   
+# don't forget    
 
 
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+
+
+class Product(models.Model):
+    product_id = models.AutoField(db_column='product_ID', unique=True)  # Field name made lowercase.
+    product_stock = models.PositiveIntegerField(db_column='product_Stock', blank=True, null=True)  # Field name made lowercase.
+    product_name = models.TextField(db_column='product_Name', blank=True, null=True)  # Field name made lowercase.
+    product_description = models.TextField(db_column='product_Description', blank=True, null=True)  # Field name made lowercase.
+    product_specifications = models.TextField(db_column='product_Specifications', blank=True, null=True)  # Field name made lowercase.
+    picture_path = models.TextField(db_column='picture_Path', blank=True, null=True)  # Field name made lowercase.
+    product_price = models.PositiveIntegerField(db_column='product_Price', blank=True, null=True)  # Field name made lowercase.
+    product_reviews = models.PositiveIntegerField(db_column='product_Reviews', blank=True, null=True)  # Field name made lowercase.
+
+    productQueryManager = ProductQuerySet.as_manager()
+	
+    class Meta:
+        managed = False
+        db_table = 'PRODUCT'
     
 
 
