9b4530a79c02a5150d847b838051fdb7d7eec64c
juliusace9000 <79270157+juliusace9000@users.noreply.github.com>
2021-11-17 22:36:48 +0100

    Small optimizations

--- backend/tuhh_campus_shop/serializers.py
+++ backend/tuhh_campus_shop/serializers.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
 from .models import *
 
+""" Auskommentiert, wird später evtl. noch benötigt
 class Catalog_request(serializers.Serializer):              ##### Arne Julius
     class Meta:
         model = ProductQuerySet
         fields = '__all__'
-
+""" 
 class Product_serializer(serializers.Serializer):
     class Meta:
-        model = product
+        model = Product
         fields = '__all__'                                  ##### Arne Julius
--- backend/tuhh_campus_shop/views.py
+++ backend/tuhh_campus_shop/views.py
@@ -1,26 +1,28 @@
 from django.shortcuts import redirect
-from .forms import NewUserForm
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
 from rest_framework.parsers import JSONParser
-from .serializers import Catalog_request
+from .serializers import Catalog_request, Product_serializer
+from rest_framework.response import Response
+from .models import Product
+
 import io
 from rest_framework.decorators import api_view
 
-
+""" Wird später evtl. noch benötigt
+@api_view(['POST'])
 def catalog_addproduct(request):                # Arne Julius
-    if request.method == 'POST':
-        json_data = request.body
-        stream = io.BytesIO(json_data)
-        python_data = JSONParser().parse(stream)
-        serializer = Catalog_request(data=python_data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-
+    json_data = request.body
+    stream = io.BytesIO(json_data)
+    python_data = JSONParser().parse(stream)
+    serializer = Catalog_request(data=python_data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+""" 
 @api_view(['GET'])                      # Arne Julius
 def catalog_display(request):   
     products= Product.product_manager.all()         # Alle Produkte sollen ausgegeben werden
-    serializer = Product_serializer(data=products)
+    serializer = Product_serializer(data=products, many=True)
     return Response(serializer.data)
